fe4b7bdd23077eaf3da0929d7729e0012f34fd7c
David Nuon <davidnuongm@gmail.com>
2015-02-09 00:28:14 -0800

    Start on web things

--- web/main.rb
+++ web/main.rb
@@ -1,18 +1,30 @@
- require 'sinatra'
+require 'sinatra'
+require "json"
 
 $:.unshift File.dirname(__FILE__) + '/../library'
 require 'warsow'
 
-before do
-    headers "Content-Type" => "application/json; charset=utf8"
+get '/' do
+    erb :index
 end
 
+post '/map' do
+	headers "Content-Type" => "application/json; charset=utf8"
+    {:message => "POST Map", 
+     :r => request.body.map}.to_json
+end
 
- get '/status' do
- 	result = ''
-	Warsow.connect '10.0.0.20', 44400, rcon: '1234' do |server| 
-		result = server.get_players
-	end
-	return result
- end
+get '/map' do
+    headers "Content-Type" => "application/json; charset=utf8"
+    {:message => "GET Map"}.to_json
+end
+
+get '/status' do
+    headers "Content-Type" => "application/json; charset=utf8"
+    result = ''
+    Warsow.connect '10.0.0.20', 44400, rcon: '1234' do |server| 
+        result = server.get_players
+    end
+    return result
+end
 
---
+++ web/public/main.js
@@ -0,0 +1,51 @@
+angular.module('tabsDemo1', ['ngMaterial'] )
+  .controller('AppCtrl', function( $scope ) {
+    $scope.next = function() {
+      $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2) ;
+    };
+    $scope.previous = function() {
+      $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
+    };
+
+
+  $scope.todos = [
+      {
+        face : '/img/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : '/img/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : '/img/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : '/img/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : '/img/list/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+    ];
+
+
+    
+  });
